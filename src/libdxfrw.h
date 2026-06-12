0659abfedba2f5948954a32395b2f8d815e37325
Thomas Beutlich <thomas.beutlich@apluss.de>
2026-06-12 19:41:45 +0200

    Fix ACAD_TABLE

--- src/libdxfrw.h
+++ src/libdxfrw.h
@@ -257,6 +257,7 @@ private:
     bool processSolid();
     bool processInsert();
     bool processAttrib(DRW_Insert* insert);
+    bool processAcadTable();
     bool processLWPolyline();
     bool processPolyline();
     bool processVertex(DRW_Polyline* pl);
